50baa69c7f1c313cc53878cf6b0e04082c4e21cd
AnlaXixDejaPris <wirtz.maxime1@gmail.com>
2023-02-04 12:11:30 +0100

    Create folders and first Scene Management scripts

--- RootsGGJ2023/Assets/Script/Menu/Menu_Start.cs
+++ RootsGGJ2023/Assets/Script/Menu/Menu_Start.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_Menu : MonoBehaviour
+public class Menu_Main : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
