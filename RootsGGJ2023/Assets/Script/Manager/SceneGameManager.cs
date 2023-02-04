6dcd80d146ddf33a7c2914d1b306403d0482fc23
AnlaXixDejaPris <wirtz.maxime1@gmail.com>
2023-02-04 15:40:25 +0100

    Menu system

---
+++ RootsGGJ2023/Assets/Script/Manager/SceneGameManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneGameManager : MonoBehaviour
+{
+    [SerializeField]
+    private ScriptableObject_Scene GameScene;
+
+    [SerializeField]
+    private ScriptableObject_Scene PlayerSelectionScene;
+
+    [SerializeField]
+    private ScriptableObject_Scene MenuStartScene;
+
+    public static SceneGameManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object !");
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        StartMenuStartScene();
+    }
+
+    private void StartMenuStartScene()
+    {
+        SceneManager.LoadScene(MenuStartScene.scene, LoadSceneMode.Additive);
+    }
+
+    public void StartPlayerSelectionScene()
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName(MenuStartScene.scene));
+        SceneManager.LoadScene(PlayerSelectionScene.scene, LoadSceneMode.Additive);
+    }
+
+    public void StartGameScene()
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneByName(PlayerSelectionScene.scene));
+        SceneManager.LoadScene(GameScene.scene, LoadSceneMode.Additive);
+    }
+}
